060bfded9441313368b8a5498b2e9a2c9d63bfa5
Quentin Dubroff <qdubroff@udel.edu>
2015-10-19 16:25:13 -0400

    Lab 3 First Commit
    Issue with your comparator

---
+++ SpringBorgataNew/src/main/java/pokerBase/Deck.java
@@ -0,0 +1,118 @@
+package pokerBase;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import pokerBase.Card;
+import pokerEnums.eRank;
+import pokerEnums.eSuit;
+
+@XmlRootElement
+public class Deck {
+	
+	private int numberOfJokers;
+	
+	@XmlElement (name="Remaining Card")
+	private ArrayList<Card> cards;
+
+	public Deck() {
+
+		//	Create an ArrayList of Cards, add each card
+		ArrayList<Card> MakingDeck = new ArrayList<Card>();
+		for (short i = 0; i <= 3; i++) {
+			eSuit SuitValue = eSuit.values()[i];			
+			for (short j = 0; j <= 12; j++) {
+				eRank RankValue = eRank.values()[j];				
+				Card NewCard = new Card(SuitValue,RankValue, (13 * i) + j+1);
+				MakingDeck.add(NewCard);
+			}
+		}
+		//	Set the instance variable
+		cards = MakingDeck;
+		ShuffleCards();
+
+	}
+	public Deck(int numberOfJokers) {
+
+		//	Create an ArrayList of Cards, add each card		
+		ArrayList<Card> MakingDeck = new ArrayList<Card>();
+		for (short i = 0; i <= 3; i++) {
+			eSuit SuitValue = eSuit.values()[i];			
+			for (short j = 0; j <= 12; j++) {
+				eRank RankValue = eRank.values()[j];				
+				Card NewCard = new Card(SuitValue,RankValue, (13 * i) + j+1);
+				MakingDeck.add(NewCard);
+			}
+		}
+		for (short i = 0; i< numberOfJokers; i++)
+		{
+			Card NewCard = new Card(eSuit.JOKER, eRank.JOKER, true);
+			MakingDeck.add(NewCard);
+		}
+		//	Set the instance variable
+		cards = MakingDeck;
+		ShuffleCards();
+	}
+	public Deck(int numOfJokers, ArrayList<Card> WildCards){
+		this(numOfJokers);
+		for(Card deckCard : cards){ //for each loop
+			for(Card WildCard : WildCards){
+				if((deckCard.getSuit() == WildCard.getSuit()) &&
+						(deckCard.getRank() == WildCard.getRank())){
+					deckCard.setWild();
+				}
+			}
+		}
+		ShuffleCards();
+	}
+
+	
+	private void ShuffleCards()
+	{
+		//	Shuffle the cards
+		Collections.shuffle(cards);
+	}
+
+	public Card drawFromDeck() {
+		// Removes the first card from the deck and return the card
+		Card FirstCard = cards.get(0);
+		cards.remove(0);
+		return FirstCard;
+	}
+
+	public int getTotalCards() {
+		// Returns the total number of cards still in the deck
+		return cards.size();
+	}
+	
+	public ArrayList<Card> getCards()
+	{
+		return this.cards;
+	}
+	
+	public StringWriter SerializeMe()
+	{
+	    StringWriter sw = new StringWriter();
+		try
+		{
+		    //Write it
+		    JAXBContext ctx = JAXBContext.newInstance(Deck.class);
+		    Marshaller m = ctx.createMarshaller();
+		    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		    m.marshal(this, sw);
+		    sw.close();			
+		}
+		catch (Exception ex)
+		{
+			
+		}
+    
+    return sw;
+	}
+}
---
+++ SpringBorgataNew/src/main/java/pokerBase/exHand.java
@@ -0,0 +1,10 @@
+package pokerBase;
+
+public class exHand extends Exception
+{
+
+	public exHand ()
+	{
+		System.out.println("It's a tie!");
+	}
+}
